25025e1dea624c54ea9dd2297436a17145a456e6
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-03 13:02:10 +0100

    installed deppendancies

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const server = express();
 
+server.use('*',(req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
